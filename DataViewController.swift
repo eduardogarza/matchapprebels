b2ca2a97fb71f5680bcdc015919e21d70a9184d7
eduardogarza <laloalejandro@gmail.com>
2016-03-12 18:19:27 -0600

    Test

---
+++ DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  matchapp
+//
+//  Created by Eduardo Alejandro Garza Sánchez on 12/03/16.
+//  Copyright © 2016 Eduardo Alejandro Garza Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
